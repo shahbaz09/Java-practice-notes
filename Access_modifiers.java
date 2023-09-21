90e0ccee0c85ca3428089540c51d5e313aea6873
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-09-22 03:22:16 +0530

    Add files via upload

---
+++ Access_modifiers.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class MyEmployee{
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public void setId(int i){
+        this.id = i;
+    }
+    public int getId(){
+        return id;
+    }
+}
+public class Access_modifiers {
+    public static void main(String[] args) {
+        MyEmployee harry = new MyEmployee();
+        // harry.id = 45;
+        // harry.name = "CodeWithHarry"; --> Throws an error due to private access modifier
+        harry.setName("CodeWithHarry");
+        System.out.println(harry.getName());
+        harry.setId(234);
+        System.out.println(harry.getId());
+    }
+}
