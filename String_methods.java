92b46bb5cb7b23a54e4fbaf0de7372d81f556c68
Shahbaz shaikh <85613707+shahbaz09@users.noreply.github.com>
2023-09-28 01:21:12 +0530

    Add files via upload

---
+++ String_methods.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+public class String_methods{
+    public static void main(String[] args){
+       String name = "Harry";
+     //  System.out.println(name);
+       int value = name.length();
+      System.out.println(value);
+     
+      // String lstring = name.toLowerCase();
+      // System.out.println(lstring);
+
+       //String UString = name.toUpperCase();
+      // System.out.println(UString);
+       
+      // String nonTrimmedString = "     Harry     ";
+      // System.out.println(nonTrimmedString);
+       
+      // String trimmedString = nonTrimmedString.trim();
+      // System.out.println(trimmedString);
+
+     // System.out.println(name.substring(1));
+    //  System.out.println(name.substring(1,5));
+
+     // System.out.println(name.replace('r','p'));
+     //  System.out.println(name.replace("r","ier"));
+
+     // System.out.println(name.startsWith("Har"));
+     //System.out.println(name.endsWith("ry"));
+     
+     //System.out.println(name.charAt(4));
+
+     //System.out.println(name.indexOf("ry"));
+
+     //System.out.println(name.equals("Harry"));
+     System.out.println("i am escape \t double quote");
+
+    }
+} 
